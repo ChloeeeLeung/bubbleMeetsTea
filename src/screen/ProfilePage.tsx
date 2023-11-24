1a1a9ea2c82c48d0ec88c72175f01a9cffb8c15a
chloeleung <chloeleung730@gmail.com>
2023-11-24 23:41:13 +0800

    feat: log out button

--- src/screen/ProfilePage.tsx
+++ src/screen/ProfilePage.tsx
@@ -1,9 +1,11 @@
+import {useNavigation} from '@react-navigation/native';
 import React from 'react';
 import {Button, Text} from 'react-native-paper';
 
 const ProfilePage = () => {
+  const navigation = useNavigation();
   return (
-    <Button mode="elevated" onPress={() => console.log('Pressed')}>
+    <Button mode="elevated" onPress={() => navigation.navigate('LoginPage')}>
       Log out
     </Button>
   );
