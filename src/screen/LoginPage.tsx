01438108788aac0a7a7ab59b8725b03b9852a43d
chloeleung <chloeleung730@gmail.com>
2024-01-21 01:33:56 +0800

    feat: login error check

--- src/screen/LoginPage.tsx
+++ src/screen/LoginPage.tsx
@@ -12,78 +12,88 @@ const LoginPage = () => {
 
   const [username, setUsername] = React.useState('');
   const [password, setPassword] = React.useState('');
+
+  const [usernameError, setUsernameError] = React.useState(false);
+  const [passwordError, setPasswordError] = React.useState(false);
   const [errorMessage, setErrorMessage] = React.useState('');
 
   const handlelogin = async () => {
     try {
       console.log('Username =>', username, ' password =>', password);
 
-      const isUserLogin = await auth().signInWithEmailAndPassword(
-        username,
-        password,
-      );
-      console.log(isUserLogin);
-
-      navigation.navigate('HomePage', {
-        username: isUserLogin.user.email,
-        uid: isUserLogin.user.uid,
-      });
+      if (username.length > 0 && password.length > 0) {
+        setUsernameError(false);
+        setPasswordError(false);
+        const isUserLogin = await auth().signInWithEmailAndPassword(
+          username,
+          password,
+        );
+        console.log(isUserLogin);
+        navigation.navigate('HomePage', {
+          username: isUserLogin.user.email,
+          uid: isUserLogin.user.uid,
+        });
+      } else if (username.length == 0 && password.length != 0) {
+        setUsernameError(true);
+        setPasswordError(false);
+        setErrorMessage('Please fill in user name.');
+      } else if (username.length != 0 && password.length == 0) {
+        setUsernameError(false);
+        setPasswordError(true);
+        setErrorMessage('Please fill in password.');
+      } else if (username.length == 0 && password.length == 0) {
+        setUsernameError(true);
+        setPasswordError(true);
+        setErrorMessage('Please fill in user name and password.');
+      }
     } catch (err) {
       console.log(err);
       setErrorMessage('' + err);
+      const errorMessage = (err as Error).message;
+      const errorCode = errorMessage.match(/\[(.*?)\]/)?.[0];
+
+      console.log(errorCode);
+      if (errorCode == '[auth/invalid-email]') {
+        setUsernameError(true);
+        setErrorMessage('User Name should be an email address.');
+      } else if (errorCode == '[auth/invalid-login]') {
+        setUsernameError(true);
+        setPasswordError(true);
+        setErrorMessage(
+          'Unable to Login. Please check your user name and password.',
+        );
+      }
     }
   };
 
   return (
     <View style={styles.centered}>
-      <Image
-        source={require('../image/bubbletea.png')}
-        style={{
-          width: 125,
-          height: 125,
-          margin: 20,
-        }}
-      />
-      <View
-        style={{
-          width: 300,
-          height: 300,
-          backgroundColor: '#C9D5BD',
-          justifyContent: 'center',
-          alignItems: 'center',
-          borderRadius: 25,
-        }}>
-        <View
-          style={{
-            width: 250,
-            margin: 5,
-          }}>
+      <Image source={require('../image/bubbletea.png')} style={styles.image} />
+      <View style={styles.errorBox}>
+        <Text style={styles.errorText}>{errorMessage}</Text>
+      </View>
+      <View style={styles.textInputBox}>
+        <View style={styles.textInput}>
           <Text>User Name</Text>
           <TextInput
             placeholder="Username"
+            error={usernameError}
             value={username}
             onChangeText={value => setUsername(value)}
-            style={{
-              backgroundColor: '#FFF8DE',
-            }}
+            style={styles.textInputcolor}
             left={<TextInput.Icon icon="account" />}
             activeUnderlineColor="#486B73"
           />
         </View>
-        <View
-          style={{
-            width: 250,
-            margin: 5,
-          }}>
+        <View style={styles.textInput}>
           <Text>Password</Text>
           <TextInput
             placeholder="Password"
             value={password}
+            error={passwordError}
             onChangeText={value => setPassword(value)}
             secureTextEntry
-            style={{
-              backgroundColor: '#FFF8DE',
-            }}
+            style={styles.textInputcolor}
             left={<TextInput.Icon icon="lock" />}
             activeUnderlineColor="#486B73"
           />
@@ -116,12 +126,6 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
   },
-  appName: {
-    fontWeight: 'bold',
-    fontSize: 30,
-    textAlign: 'center',
-    fontFamily: 'Caveat',
-  },
   buttonSize: {
     paddingVertical: 12,
     width: 200,
@@ -132,6 +136,32 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
     color: '#ffffff',
     fontFamily: 'Caveat',
+  },
+  textInputBox: {
+    width: 300,
+    height: 300,
+    backgroundColor: '#C9D5BD',
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 25,
+  },
+  textInput: {
+    width: 250,
+    margin: 5,
+  },
+  textInputcolor: {
+    backgroundColor: '#FFF8DE',
+  },
+  image: {
+    width: 125,
+    height: 125,
+    margin: 10,
+  },
+  errorBox: {
+    width: 250,
+  },
+  errorText: {
+    color: 'red',
   },
 });
 
