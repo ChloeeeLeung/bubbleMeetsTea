8f398eab18e3efdfabcee031829e30832cfd38e6
chloeleung <chloeleung730@gmail.com>
2024-04-09 23:12:05 +0800

    feat: show time

--- src/component/shopInfo.tsx
+++ src/component/shopInfo.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import {StyleSheet, View} from 'react-native';
 import {Text, Button} from 'react-native-paper';
 import Icon from 'react-native-vector-icons/FontAwesome';
@@ -16,16 +16,38 @@ export default function ShopInfo({
   telephone: String;
   navigation: any;
 }) {
+  const [hourNow, setHourNew] = useState('');
+
+  useEffect(() => {
+    const time = new Date().getTime();
+    const date = new Date(time);
+    const hour = date.getHours();
+
+    const openHour = parseInt(openTime.split(':')[0], 10);
+    const closeHour = parseInt(closeTime.split(':')[0], 10);
+    if (
+      openHour <= hour && closeHour < 5 ? hour > closeHour : hour < closeHour
+    ) {
+      const nextHour = hour < 23 ? hour + 1 : hour + 1 - 24;
+      const formattedHour = String(hour).padStart(2, '0') + ':00';
+      const formattedNextHour = String(nextHour).padStart(2, '0') + ':00';
+
+      setHourNew('Now (' + formattedHour + '-' + formattedNextHour + '): Busy');
+    } else {
+      setHourNew('Closed');
+    }
+  }, []);
+
   return (
     <View>
       <View style={styles.infoText}>
         <View style={styles.row}>
           <Icon name="sun-o" size={23} color={'#2f4858'} />
-          <Text style={{marginLeft: 5}}>Open at {openTime}</Text>
+          <Text style={styles.textMargin}>Open at {openTime}</Text>
         </View>
         <View style={styles.row}>
           <Icon name="moon-o" size={23} color={'#2f4858'} />
-          <Text style={{marginLeft: 5}}>Close at {closeTime}</Text>
+          <Text style={styles.textMargin}>Close at {closeTime}</Text>
         </View>
       </View>
       <Button
@@ -37,14 +59,18 @@ export default function ShopInfo({
       </Button>
       <View style={styles.row}>
         <Icon name="map-marker" size={23} color={'#2f4858'} />
-        <Text style={{marginLeft: 5}}>{location}</Text>
+        <Text style={styles.textMargin}>{location}</Text>
       </View>
       {telephone && (
         <View style={styles.row}>
           <Icon name="phone" size={23} color={'#2f4858'} />
-          <Text style={{marginLeft: 5}}>{telephone}</Text>
+          <Text style={styles.textMargin}>{telephone}</Text>
         </View>
       )}
+      <View style={styles.row}>
+        <Icon name="bullseye" size={23} color={'#2f4858'} />
+        <Text style={styles.textMargin}>{hourNow}</Text>
+      </View>
     </View>
   );
 }
@@ -59,5 +85,8 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     height: 40,
     paddingHorizontal: 15,
+  },
+  textMargin: {
+    marginLeft: 5,
   },
 });
